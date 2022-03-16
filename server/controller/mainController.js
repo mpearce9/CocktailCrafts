47aa8a8b554bf95640918657e67711859c5af472
Gabriel Novack <54568509+gnovackUNCC@users.noreply.github.com>
2022-03-16 11:38:28 -0400

    Added vue, seperated front and backend to folders

---
+++ server/controller/mainController.js
@@ -0,0 +1,11 @@
+exports.home = (req, res)=>{
+    res.render('home');
+};
+
+exports.contactUs = (req, res)=>{
+    res.render('contactUs');
+};
+
+exports.aboutUs = (req, res)=>{
+    res.render('aboutUs');
+};
