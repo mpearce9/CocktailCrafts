8d1c2a884571433ff2f3b595b73703b9d928fc06
Michael <mpearce9@uncc.edu>
2022-03-23 19:44:08 -0400

    Created about and discover page

---
+++ frontend/src/components/AboutUs.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.jpg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Cocktail Crafts...
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Cocktail Crafts was founded by five UNC Charlotte students. At Cocktail
+          <br>
+         Crafts we are passionate about the discovery of new recipes, trying new
+         <br>
+         new things, and going out of your comfort zone. Navigate through our site 
+         <br>
+         to learn more about cocktails, use our different features. Have a question?
+         <br>
+         Head over to our contact us tab and get in touch!
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
---
+++ frontend/src/views/discoverComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app>
+    <v-container>
+    <v-row
+      class="mb-8"
+    >
+        <v-card
+        color = "rgb(15,12,12)"
+        id = "homecard"
+        >
+          <div>
+            <v-row>
+            <h1 style = "font-size:2.0em;">Discover your favorite cocktail today...</h1>
+            </v-row>
+            <v-row>
+                <h1>&zwnj;</h1>
+            </v-row>
+            <v-row>
+            <v-col cols="4">
+                <v-btn block color = "info" href = "/popular">Popular</v-btn>
+            </v-col>
+            <v-col>
+                <h1>&zwnj;</h1>
+            </v-col>
+            <v-col cols="4">
+                <v-btn block color = "info" href = "/ingredients">Ingredients</v-btn>
+            </v-col>
+            <v-col>
+                <h1>&zwnj;</h1>
+            </v-col>
+            <v-col cols="4">
+                <v-btn block color = "info" href = "/search">Search</v-btn>
+            </v-col>
+            </v-row>
+        </div>
+        </v-card>
+    </v-row>
+    </v-container>
+    </v-app>
+</template>
