abc5d21da17a6adb759b87cee50b64d789394a26
AnaPathak <89747332+AnaPathak@users.noreply.github.com>
2022-04-07 07:29:52 -0400

    initial commit for barShelf

---
+++ frontend/src/views/barShelf.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+    <v-container>
+    <v-row
+      class="mb-8"
+    >
+        <v-card
+        color = "rgb(15,12,12)"
+        id = "barShelf"
+        width = "100%"
+        justify-center
+        >
+          <div>
+            <v-row>
+                <h1 style = "font-size:2.0em; color:blue;">Your Bar Shelf</h1>
+                <hr style = "size:20; color: blue;">
+            </v-row>
+          </div>
+        </v-card>
+    </v-row>
+    </v-container>
+    </v-app>
+</template>
