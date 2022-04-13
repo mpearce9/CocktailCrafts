ed87760ef2a4d5c8bdf0a1177300bea8e0c58847
Michael Pearce <89855072+mpearce9@users.noreply.github.com>
2022-04-13 12:01:13 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/CocktailCrafts/'
