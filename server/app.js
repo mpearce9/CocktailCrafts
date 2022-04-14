b983487e0930c53bd72135809164ca5e75b0e824
Michael Pearce <89855072+mpearce9@users.noreply.github.com>
2022-04-14 18:49:42 -0400

    Fixed app.js, removed old/unused files

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 //require modules
 const express = require('express');
 const morgan = require('morgan');
-const methodOverride = require('method-override');
 const main = require('./routes/main');
 const { ppid } = require('process');
 const mongoose = require ('mongoose');
@@ -14,20 +13,14 @@ var bodyParser = require('body-parser');
 const app = express();
 
 //configure appm
-let port = process.env.PORT || 3000;
-let host = 'localhost';
-app.set('view engine', 'ejs');
+let port = process.env.PORT || 3001;
 
 //mount middleware
-app.use(express.static('public'));
 app.use(express.static('dist'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'));
-app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.use('/public', express.static('public'));
 
 const db = require("./models");
 db.mongoose
@@ -74,8 +67,6 @@ app.get('/*', (req, res)=> {
     res.sendFile(__dirname + '/dist/index.html')
 })
 
-app.listen(3000);
-
 
 app.use((req, res, next)=> {
     let err = new Error('The server cannot locate ' + req.url);
@@ -88,8 +79,7 @@ app.use((err, req, res, next)=>{
         err.status = 500;
         err.message = ("Internal server error");
     }
-    res.status(err.status);
-    res.render('error', {error: err});
+    res.status(err.status).json({error: err});
 });
 
 //start the server
