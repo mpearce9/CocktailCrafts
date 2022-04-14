3103a414fc6597c108c015e5f8c0095eb75a88fd
Michael Pearce <89855072+mpearce9@users.noreply.github.com>
2022-04-14 19:13:16 -0400

    Changed app.js and deploy.yml, updated gitignore

--- server/app.js
+++ server/app.js
@@ -65,15 +65,8 @@ app.use(cors());
 app.use('/api/', routes);
 
 app.get('/*', (req, res)=> {
-    res.sendFile(path.join('dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
-
-
-app.use((req, res, next)=> {
-    let err = new Error('The server cannot locate ' + req.url);
-    err.status = 404;
-    next(err);
-});
 
 app.use((err, req, res, next)=>{
     if(!err.status) {
