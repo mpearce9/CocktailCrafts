4aa69e889e8dd17fd68248e564bd73314a320995
Michael Pearce <89855072+mpearce9@users.noreply.github.com>
2022-05-05 10:19:33 -0400

    Revert "New bar shelf"

--- frontend/src/views/accountDetailsComponent.vue
+++ frontend/src/views/accountDetailsComponent.vue
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-<template>
-  <v-container style="background-color:black">
-    <v-row class="text-center">
-
-      <v-col class="mb-4">
-        <h2 style="color:white">
-          Account Details
-        </h2>
-
-        <form style="background-color:#3760D6; margin:auto; width: 400px">
-          <p style="color:white">Email: </p>
-          <input type="email" size="20" style="box-shadow: 1px 1px 2px 2px;" />
-          <br> <br>
-          <p style="color:white">Name: </p>
-          <input type="text" size="20" style="box-shadow: 1px 1px 2px 2px;" />
-          <br><br>
-          <p style="color:white">Activities: </p>
-          <select  style="width: 180px; background-color: white; box-shadow: 1px 1px 2px 2px; color:black" >
-              <option style="color:black">Activity 1</option>
-              <option style="color:black">Activity 2</option>
-              <option style="color:black">Activity 3</option>
-              <option style="color:black">Activity 4</option>
-          </select>
-            <br>
-        </form>
-
-      </v-col>
-
-    </v-row>
-  </v-container>
-</template>
-diff --git a/frontend/src/router/index.js b/frontend/src/router/index.js
-=======
 <template>
   <v-container style="background-color:black">
     <!-- these account details are shown based on the current logged in user -->
@@ -65,4 +31,3 @@ diff --git a/frontend/src/router/index.js b/frontend/src/router/index.js
   </v-container>
 </template>
 diff --git a/frontend/src/router/index.js b/frontend/src/router/index.js
->>>>>>> 4867fcfe9855869b54a2eb2df697ea4acbd98969
--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,6 @@ const express = require('express');
 const controller = require('../controller/mainController');
 const router = express.Router();
 const usercontroller = require('../controller/user.controller');
-const ingredientsController = require('../controller/ingredientsController');
 
 // these routes are for the api routes from our app url, based on the url, routes to the controllers and gets their specific functions
 // contains api functions and user functions
@@ -29,18 +28,4 @@ router.get('/listfavorites', usercontroller.listfavorites)
 
 router.get('/isfavorite/:id', usercontroller.isfavorite)
 
-<<<<<<< HEAD
-router.post('/addIngredients', ingredientsController.addIngredients)
-
-router.post('/listAddedIngredients', ingredientsController.listAddedIngredients)
-/*
-// error handler
-router.use(function(err, req, res, next) {
-  console.log('error handling');
-  res.status(err.status || 500);
-  res.render('error',{error:err,message:err.message,url:req.url});
-});
-*/
-=======
->>>>>>> 4867fcfe9855869b54a2eb2df697ea4acbd98969
 module.exports = router;
